3740197b34d6f3a7dacf4612d2a972d6d3763650
Kinga Wrona <kingawr123@gmail.com>
2023-05-31 17:04:29 +0200

    printing list

--- tua/interpreter/builtins.py
+++ tua/interpreter/builtins.py
@@ -2,8 +2,14 @@ from .visitor import Tua
 from .variables import Value
 
 def print_(_: Tua, *args: Value):
-    printables = list(map(lambda arg: arg.value, args))
-    print(*printables)
+    printables = []
+    if len(args) > 0 and "List" in args[0].type.id:
+        printables = list(map(lambda arg: arg.value, args[0].value))
+        print(printables)    
+    else: 
+        printables = list(map(lambda arg: arg.value, args))
+        print(*printables)
+
 
 def dump_stack(visitor: Tua):
     print(f"Stack: ", visitor.scope)
