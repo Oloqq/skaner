b8088624a651b4c8ed15609ba96e369f08f031e6
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-06-14 09:20:20 +0200

    multiline

--- tua/interpreter/main.py
+++ tua/interpreter/main.py
@@ -12,41 +12,41 @@ def multiline_triggered(line: str) -> bool:
     words = line.split(" ")
     return words[0] == "function" or words[-1] in ["then", "do"]
 
+# limitations:
+# at most one new scope per line
+# end must be on it's own line (unless scope was opened at the same line)
+def handle_multiline(line):
+    level = 1
+    buffer = [line]
+    while True:
+        line = input(">").strip()
+        buffer.append(line)
+        if multiline_triggered(line):
+            level += 1
+        if line == "end":
+            level -= 1
+        if level <= 0:
+            break
+    return " ".join(buffer)
+
 def run_interpreter_line_by_line():
     visitor = Tua()
 
     print(">>>", end="")
 
     for line in iter(input, ''):
-        code = line
+        if line == "exit":
+            break
+        elif multiline_triggered(line):
+            code = handle_multiline(line)
+        else:
+            code = line
+
         lexer = TuaLexer(InputStream(code))
         tokens = CommonTokenStream(lexer)
         parser = TuaParser(tokens)
         tree = parser.program()
         visitor.visit(tree)
-
-        # Check if the current line ends with a trigger word,
-        # indicating that we're entering multi-line mode.
-        # if any(line.strip().endswith(trigger) for trigger in multi_line_triggers):
-        #     multiline_mode = True
-
-        # # Check if we're in multi-line mode and the line ends with 'end',
-        # # indicating that the current statement is complete.
-        # if multiline_mode and line.strip().endswith('end'):
-        #     # Concatenate all lines in the buffer and send them to the lexer and parser.
-        #     code = ''.join(buffer)
-        #     lexer = TuaLexer(InputStream(code))
-        #     tokens = CommonTokenStream(lexer)
-        #     parser = TuaParser(tokens)
-
-        #     # Build the parse tree and visit it.
-        #     tree = parser.program()
-        #     visitor.visit(tree)
-
-        #     # Clear the buffer and exit multi-line mode.
-        #     buffer.clear()
-        #     multiline_mode = False
-
         print('>>> ', end='')
 
 def run_interpreter_full_program(program: FileStream|InputStream):
@@ -65,7 +65,7 @@ def run_interpreter_full_program(program: FileStream|InputStream):
 def run_interpreter(input_file, debug):
     init_log(logging.DEBUG if debug else logging.WARNING)
     if input_file != None:
-        run_interpreter_full_program(FileStream(input_file))    
+        run_interpreter_full_program(FileStream(input_file))
     else:
         run_interpreter_line_by_line()
 
