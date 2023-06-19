2e07e2783ec7d17d02419994cf03e03a8aa3662d
Olgierd Piofczyk <olgierdpk@gmail.com>
2023-06-19 18:05:32 +0200

    bulletproof errorlistener

--- tua/interpreter/main.py
+++ tua/interpreter/main.py
@@ -15,6 +15,15 @@ class CustomErrorListener:
     def syntaxError(self, recognizer, offendingSymbol, line, column, msg, e):
         self.errors.append(f"Syntax error at line {line}, column {column}: {msg}")
 
+    def reportAmbiguity(self, recognizer, dfa, startIndex, stopIndex, exact, ambigAlts, configs):
+        pass
+
+    def reportAttemptingFullContext(self, recognizer, dfa, startIndex, stopIndex, conflictingAlts, configs):
+        pass
+
+    def reportContextSensitivity(self, recognizer, dfa, startIndex, stopIndex, prediction, configs):
+        pass
+
 
 def multiline_triggered(line: str) -> bool:
     words = line.split(" ")
